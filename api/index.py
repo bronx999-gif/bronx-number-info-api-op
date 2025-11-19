f90724b7fad04f3e6e50987c99b47ce1855a5b18
bronx999-gif <channelgreenplay@gmail.com>
2025-11-19 07:35:13 +0530

    index.py

--- api/index.py
+++ api/index.py
@@ -4,7 +4,34 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return '{
+  "success": true,
+  "result": [
+    {
+      "id": 1205627021,
+      "mobile": "7676184626",
+      "name": "Shivalingappa ",
+      "father_name": "Durugappa ",
+      "address": "S/O: Durugappa!257!!harijana colony!nadanga  !Nadanga!Bellary!Karnataka!583114",
+      "alt_mobile": "",
+      "circle": "AIRTEL KARNATKA",
+      "id_number": "",
+      "email": ""
+    },
+    {
+      "id": 1205627022,
+      "mobile": "7676184626",
+      "name": "Shivalingappa",
+      "father_name": "Durugappa",
+      "address": "S/O  Durugappa!!257 harijana colony!nadanga!Nadanga Bellary!Karnataka!583114",
+      "alt_mobile": "919901542381",
+      "circle": "KARNATAKA JIO ",
+      "id_number": "",
+      "email": ""
+    }
+  ],
+  "credit": "@BRONX_ULTRA"
+}'
 
 @app.route('/about')
 def about():
